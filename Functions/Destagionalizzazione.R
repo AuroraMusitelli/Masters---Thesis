45cca50e6d52e30c64b16e5fea4ba119295f402a
Aurora Musitelli <musitelliaurora@gmail.com>
2025-11-19 13:26:00 +0100

    Update Destagionalizzazione.R

--- Functions/Destagionalizzazione.R
+++ Functions/Destagionalizzazione.R
@@ -1,64 +1,44 @@
-## ----- 6.1 Funzione per verificare stagionalità delle serie storiche -----
-ha_stagionalita <- function(ts_data, soglia = 0.3) {
-  acf_vals <- acf(ts_data, lag.max = 24, plot = FALSE)$acf
-  return(abs(acf_vals[13]) > soglia)}  # lag 12 per dati mensili
-
-
-## ----- 6.2 Funzione per destagionalizzazione con dummy mensili -----
-destag_dummies <- function(ts_data) {
-  if (!is.ts(ts_data)) stop("Input deve essere un oggetto ts")
-  freq <- frequency(ts_data)
-  month_factor <- factor(cycle(ts_data))
-  fit <- lm(as.numeric(ts_data) ~ month_factor)
-  residui <- resid(fit)
-  return(ts(residui, start = start(ts_data), frequency = freq))}
-
-
-## ----- 6.3 Funzione per applicare destagionalizzazione per ogni serie -----
-applica_destag_dummies <- function(ts_matrix) {
-  destag_list <- list()
-  info_list <- list()
+#### FUNZIONE: Destagionalizzazione X-13 per tsibble      
+destag_tsibble <- function(tsib) {
   
-  for (serie in colnames(ts_matrix)) {
-    ts_col <- ts_matrix[, serie]
-    if (all(is.na(ts_col))) next
+  tmp <- as_tibble(tsib)
+  cols <- tmp %>%
+    dplyr::select(-Month, -Date_idx) %>%
+    names()
+  
+  message("Destagionalizzazione in corso per ", length(cols), " serie...")
+  
+  for (col in cols) {
+    cat("processo:", col, "\n")
     
-    stagionale <- ha_stagionalita(ts_col)
-    if (stagionale) {
-      ts_destag <- destag_dummies(ts_col)
-      destag_list[[serie]] <- ts_destag
-      info_list[[serie]] <- TRUE
-    } else {
-      destag_list[[serie]] <- ts_col
-      info_list[[serie]] <- FALSE
+    serie <- tmp[[col]]
+    serie <- as.numeric(serie)
+    
+    # interpolazione minima per NA (X-13 non li accetta)
+    if (anyNA(serie)) {
+      serie <- na.approx(serie, na.rm = FALSE)
     }
+    
+    # costruisco la serie ts mensile
+    ts_obj <- ts(serie, start = c(year(tmp$Month[1]), month(tmp$Month[1])), freq = 12)
+    
+    # applico destagionalizzazione X-13 ARIMA-SEATS
+    tryCatch({
+      
+      fit <- seas(ts_obj)
+      sa <- fit$series$s11   # serie destagionalizzata
+      
+      tmp[[col]] <- as.numeric(sa)
+      
+    }, error = function(e) {
+      warning("Errore con la serie ", col, ": la lascio originale (nessuna destagionalizzazione).")
+    })
   }
-  destag_matrix <- do.call(cbind, destag_list)
-  destag_matrix <- ts(destag_matrix, start = start(ts_matrix), frequency = frequency(ts_matrix))
   
-  tabella_info <- tibble(
-    Serie = names(info_list),
-    Stagionale = unlist(info_list)
-  )
-  return(list(data = destag_matrix, info = tabella_info))}
+  # ricostruisco tsibble
+  tsib_out <- tmp %>%
+    as_tsibble(index = Date_idx)
+  
+  return(tsib_out)
+}
 
-
-## ----- 6.4 Funzione per grafico unico di tutte le serie storiche -----
-plot_tutte_destag <- function(ts_originale, ts_destag, titolo = "Serie Originali vs Destagionalizzate") {
-  time_index <- time(ts_originale)
-  df_originale <- as.data.frame(ts_originale)
-  df_destag <- as.data.frame(ts_destag)
-  df_originale$Time <- df_destag$Time <- time_index
-  
-  df_long <- bind_rows(
-    pivot_longer(df_originale, -Time, names_to = "Serie", values_to = "Valore") %>%
-      mutate(Tipo = "Originale"),
-    pivot_longer(df_destag, -Time, names_to = "Serie", values_to = "Valore") %>%
-      mutate(Tipo = "Destagionalizzata"))
-  
-  ggplot(df_long, aes(x = Time, y = Valore, color = Serie, linetype = Tipo)) +
-    geom_line(linewidth = 0.8) +
-    labs(title = titolo, x = "Tempo", y = "Valore") +
-    theme_minimal() +
-    theme(legend.position = "bottom", legend.text = element_text(size = 8)) +
-    guides(color = guide_legend(ncol = 2))}
